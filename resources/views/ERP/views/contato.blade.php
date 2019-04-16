d6b602bce6efd8ab25141ba6d9f1b44fbbb4c487
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2019-04-15 23:18:15 -0300

    :clipboard:

--- resources/views/ERP/views/contato.blade.php
+++ resources/views/ERP/views/contato.blade.php
@@ -1,4 +1,12 @@
-Contatos
+@extends('ERP.base.header')
+
+@section('title', 'Contatos')
+
+@section('content')
+
+<div class="container">
 @foreach ($registros as $registro)
   <p>{{ $registro->id }}</p>
 @endforeach
+</div>
+@stop
